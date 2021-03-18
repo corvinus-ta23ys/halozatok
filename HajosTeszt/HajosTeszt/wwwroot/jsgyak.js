c39991b1b84baf42b167b49609c3e76071f38063
Gábor Szanyi <gabor.szanyi@stud.uni-corvinus.hu>
2021-03-18 22:49:48 +0100

    js_v1

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,13 @@
+﻿window.onload = () => {
+    let belso = document.getElementById("kulso");
+    belso.innerHTML = "";
+    belso.classList.add("egymas_melle");
+    for (var i = 0; i < 10; i++) {
+        let szam = document.createElement("div");
+        belso.appendChild(szam);
+        szam.innerText = (i + 1);
+        szam.classList.add("doboz");
+        szam.style.color = 'rgb(${255/10*(10-i)},0,0)'
+        
+    }
+}
