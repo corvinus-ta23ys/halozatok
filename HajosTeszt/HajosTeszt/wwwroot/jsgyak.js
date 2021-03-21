076d020b057932c356b2cf68affa214981c18285
Gábor Szanyi <gabor.szanyi@stud.uni-corvinus.hu>
2021-03-21 17:09:41 +0100

    pascal

--- HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -7,7 +7,29 @@
         belso.appendChild(szam);
         szam.innerText = (i + 1);
         szam.classList.add("doboz");
-        szam.style.color = 'rgb(${255/10*(10-i)},0,0)'
-        
+        szam.style.color = `rgb(${255 - (255 / 10 * i)},${255 - (255 / 10 * i)},${255 - (255 / 10 * i)})`
+
+    }
+
+    var faktorialisR = (n) => {
+        if (n === 0 || n === 1) {
+            return 1;
+        }
+        else {
+            return n * faktorialisR(n - 1);
+        }
+    }
+
+
+    for (var sor = 0; sor < 10; sor++) {
+        var sorok = document.createElement("div");
+        sorok.classList.add("sor");
+        document.getElementById("pascal").appendChild(sorok);
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            var oszlopok = document.createElement("div");
+            sorok.appendChild(oszlopok);
+            oszlopok.classList.add("elem");
+            oszlopok.innerText = faktorialisR(sor) / (faktorialisR(oszlop) * faktorialisR(sor - oszlop));
+        }
     }
 }
